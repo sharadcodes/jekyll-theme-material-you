b4bec9768276907e47e3bce70af8de07f8a4c860
Sharad Raj <dev_sharad@outlook.com>
2022-07-22 07:55:16 +0530

    Fix bugs: Whitespace problem

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -1,12 +1,15 @@
 ---
 ---
 
-const categories = { {% for category in site.categories %}{% capture category_name %}{{ category | first }}{% endcapture %}{{ category_name }}: [{% for post in site.categories[category_name] %}{ url: `{{ site.baseurl }}{{ post.url }}`, date: `{{post.date | date_to_string}}`, title: `{{post.title}}`},{% endfor %}],{% endfor %} }
+const categories = { {% for category in site.categories %}{% capture category_name %}{{ category | first }}{% endcapture %}{{ category_name | replace: " ", "_" }}: [{% for post in site.categories[category_name] %}{ url: `{{ site.baseurl }}{{ post.url }}`, date: `{{post.date | date_to_string}}`, title: `{{post.title}}`},{% endfor %}],{% endfor %} }
+
+console.log(categories);
 
 window.onload = function () {
-  document.querySelectorAll(".category").forEach((category) => {
+  document.querySelectorAll(".article-category").forEach((category) => {
     category.addEventListener("click", function (e) {
-      const posts = categories[e.target.innerText];
+      const posts = categories[e.target.innerText.replace(" ","_")];
+      console.log(posts);
       let html = ``
       posts.forEach(post=>{
         html += `
