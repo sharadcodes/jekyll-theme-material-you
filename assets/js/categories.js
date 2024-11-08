9c245210c658c5f2dc15efa2d5624a3e79f662f7
S.KIM <102121954+selinakk@users.noreply.github.com>
2024-11-08 16:24:42 +0900

    Fix issue with category names containing dots (.) (#7)

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -1,7 +1,7 @@
 ---
 ---
 
-const categories = { {% for category in site.categories %}{% capture category_name %}{{ category | first }}{% endcapture %}{{ category_name | replace: " ", "_" }}: [{% for post in site.categories[category_name] %}{ url: `{{ site.baseurl }}{{ post.url }}`, date: `{{post.date | date_to_string}}`, title: `{{post.title}}`},{% endfor %}],{% endfor %} }
+const categories = { {% for category in site.categories %}{% capture category_name %}{{ category | first }}{% endcapture %}"{{ category_name | replace: " ", "_" }}": [{% for post in site.categories[category_name] %}{ url: `{{ site.baseurl }}{{ post.url }}`, date: `{{post.date | date_to_string}}`, title: `{{post.title}}`},{% endfor %}],{% endfor %} }
 
 console.log(categories);
 
